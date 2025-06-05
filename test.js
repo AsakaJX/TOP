555e605534f9675c1d782b3fc0863adc6662f8c1
AsakaJX <yourasaka@protonmail.com>
2025-06-05 20:46:59 +0300

    Add basic web files, Remove meow.txt

---
+++ test.js
@@ -0,0 +1,23 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function checkWord(word) {
+    let tempStr = '';
+    
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i];
+
+        if (letter == ' ') { continue; }
+        
+        if (tempStr.indexOf(letter) == -1) {
+            tempStr += letter;
+        }
+    }
+    
+    return tempStr.length == alphabet.length ?
+        'includes every letter of alphabet' :
+        'doesn\'t includes every letter of alphabet';
+}
+
+const word = 'The quick brown fox jumps over the lazy dog';
+
+console.log(`word: ${word} ` + checkWord(word));
